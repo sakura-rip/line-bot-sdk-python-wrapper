b3b184ac864824fe05f3b5f22b1903adfd506de7
ch31212y <okaoka1567@gmail.com>
2020-10-31 18:15:15 +0900

    format: format import

--- linebot_wrapper/api/message_struct.py
+++ linebot_wrapper/api/message_struct.py
@@ -1,7 +1,14 @@
 from dataclasses import dataclass
 
-from linebot.models import ImageSendMessage, QuickReply, VideoSendMessage, TextSendMessage, AudioSendMessage, \
-    LocationSendMessage, StickerSendMessage
+from linebot.models import (
+    ImageSendMessage,
+    QuickReply,
+    VideoSendMessage,
+    TextSendMessage,
+    AudioSendMessage,
+    LocationSendMessage,
+    StickerSendMessage
+)
 
 
 @dataclass

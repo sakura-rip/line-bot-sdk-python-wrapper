d6f5ba48560a1bf99e8a345690697126911d7ffa
ch31212y <okaoka1567@gmail.com>
2020-10-31 19:05:05 +0900

    rename: service class

--- linebot_wrapper/client.py
+++ linebot_wrapper/client.py
@@ -1,15 +1,15 @@
 from linebot import LineBotApi, WebhookParser
 
 from .api.pollservice import PollService
-from .api.talkservice import TalkService
+from .api.messageservice import MessageService
 
 
-class LineClient(TalkService, PollService):
+class LineClient(MessageService, PollService):
     def __init__(self, access_token: str, secret: str):
         self.bot = LineBotApi(access_token)
         self.parser = WebhookParser(secret)
         self.init_all()
 
     def init_all(self):
-        TalkService.__init__(self)
+        MessageService.__init__(self)
         PollService.__init__(self)

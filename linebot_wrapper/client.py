15ca606d436383153c3638515b3938ad5d151eb2
ch31212y <okaoka1567@gmail.com>
2020-10-31 19:15:51 +0900

    change: Inherit LineBotApi

--- linebot_wrapper/client.py
+++ linebot_wrapper/client.py
@@ -4,12 +4,13 @@ from .api.pollservice import PollService
 from .api.messageservice import MessageService
 
 
-class LineClient(MessageService, PollService):
+class LineClient(MessageService, PollService, LineBotApi):
     def __init__(self, access_token: str, secret: str):
         self.api = LineBotApi(access_token)
         self.parser = WebhookParser(secret)
-        self.init_all()
+        self.init_all(access_token)
 
-    def init_all(self):
+    def init_all(self, access_token):
+        LineBotApi.__init__(self, access_token)
         MessageService.__init__(self)
         PollService.__init__(self)

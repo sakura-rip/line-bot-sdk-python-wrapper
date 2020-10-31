153aee563b7a4e4b6ca4a4bb87eec2816143c67e
ch31212y <okaoka1567@gmail.com>
2020-10-31 16:29:57 +0900

    add: comment

--- linebot_wrapper/api/poll.py
+++ linebot_wrapper/api/poll.py
@@ -8,14 +8,24 @@ class Poll:
     def __init__(self):
         self.op_interrupts = {}
 
-    def handle_operation(self, body, signature):
+    def handle_operation(self, body: str, signature: str):
+        """Operation Handler
+        LINEから飛んできたWebhookをParseし、Wrapper向けに書き直す
+        :param body: string 飛んできたWebHookの内容
+        :param signature: string シグネチャー
+        :return: None
+        """
         ops = self.parser.parse(body, signature)
         for op in ops:
             if op.type not in self.op_interrupts:
                 continue
             self.execute_func(op)
 
-    def execute_func(self, op):
+    def execute_func(self, op: Event):
+        """func executer
+        :param op: parsed Event from Webhook
+        :return: None
+        """
         assert op.type not in self.op_interrupts, f"{op.type} is already added to interrupts"
         try:
             self.op_interrupts[op.type](self.bot, op)
@@ -23,8 +33,17 @@ class Poll:
             print_exc()
 
     def add_op_interrupt(self, op: Event, func: Callable):
+        """ Add Event to handler
+        :param op: Event which you want to handle
+        :param func: Function which you want to call
+        :return: None
+        """
         self.op_interrupts[op.type] = func
 
     def add_op_interrupts(self, dicts: Dict[Event, Callable]):
+        """Add Events to handler
+        :param dicts: Dict[Event, func]
+        :return: None
+        """
         for event, fnc in dicts.items():
             self.op_interrupts[event.type] = fnc

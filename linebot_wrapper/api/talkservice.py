b2e16fb7b95c259aef80312b01c760ac5e0e81a4
ch31212y <okaoka1567@gmail.com>
2020-10-31 18:13:41 +0900

    rename: reply_message to reply_message_base

--- linebot_wrapper/api/talkservice.py
+++ linebot_wrapper/api/talkservice.py
@@ -9,7 +9,7 @@ class TalkService:
     def __init__(self):
         self.reply_token = None
 
-    def reply_message(self, messages: Union[SendMessage, List[SendMessage]]):
+    def reply_message_base(self, messages: Union[SendMessage, List[SendMessage]]):
         """Reply Message
         LINEからできたEventに対する返信を行います
         :param messages: Message object which you want to sent
@@ -25,7 +25,7 @@ class TalkService:
         :param texts: Text object which you want to send
         :return: None
         """
-        self.reply_message(
+        self.reply_message_base(
             [TextSendMessage(text=text) if isinstance(text, str) else text.create_message()
              for text in texts]
         )
@@ -36,7 +36,7 @@ class TalkService:
         :param images: ImageMessage object which you want to send
         :return: None
         """
-        self.reply_message(
+        self.reply_message_base(
             [image.create_message() for image in images]
         )
 
@@ -46,21 +46,21 @@ class TalkService:
         :param videos:  VideoMessage object which you want to send
         :return: None
         """
-        self.reply_message(
+        self.reply_message_base(
             [video.create_message() for video in videos]
         )
 
     def reply_audio(self, *audios: Union[AudioMessage, List[AudioMessage]]):
-        self.reply_message(
+        self.reply_message_base(
             [audio.create_message() for audio in audios]
         )
 
     def reply_location(self, *locations: Union[LocationMessage, List[LocationMessage]]):
-        self.reply_message(
+        self.reply_message_base(
             [location.create_message() for location in locations]
         )
 
     def reply_sticker(self, *stickers: Union[StickerMessage, List[StickerMessage]]):
-        self.reply_message(
+        self.reply_message_base(
             [sticker.create_message() for sticker in stickers]
         )
